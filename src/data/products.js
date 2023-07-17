c9df312322b9e3db5825014e6b55afe1a36de60e
janko12 <henrich.sury97@gmail.com>
2023-07-17 17:35:41 +0200

    Create products data

---
+++ src/data/products.js
@@ -0,0 +1,126 @@
+import {
+    i10,
+    i10s,
+    i11,
+    i11pro,
+    i12,
+    i12pro,
+    i13,
+    i13mini,
+    i13pro,
+    i6,
+    i6s,
+    i6splus,
+    i7,
+    i7plus,
+    i8,
+    i8plus,
+    i9,
+} from "./productsImages";
+
+const products = [
+    {
+        title: "Iphone 6",
+        image: i6,
+        description: "Awesome iphone 6",
+        price: 300,
+    },
+    {
+        title: "Iphone 6s",
+        image: i6s,
+        description: "Awesome iphone 6 s",
+        price: 280,
+    },
+    {
+        title: "Iphone 6s plus",
+        image: i6splus,
+        description: "Awesome iphone 6 s plus",
+        price: 340,
+    },
+    {
+        title: "Iphone 7",
+        image: i7,
+        description: "Awesome iphone 7",
+        price: 370,
+    },
+    {
+        title: "Iphone 7 plus",
+        image: i7plus,
+        description: "Awesome iphone 7 plus",
+        price: 400,
+    },
+    {
+        title: "Iphone 8",
+        image: i8,
+        description: "Awesome iphone 8 plus",
+        price: 440,
+    },
+    {
+        title: "Iphone 8 plus",
+        image: i8plus,
+        description: "Awesome iphone 8 plus",
+        price: 480,
+    },
+    {
+        title: "Iphone 9",
+        image: i9,
+        description: "Awesome iphone 9",
+        price: 520,
+    },
+    {
+        title: "Iphone 10",
+        image: i10,
+        description: "Awesome iphone 10",
+        price: 620,
+    },
+    {
+        title: "Iphone 10s",
+        image: i10s,
+        description: "Awesome iphone 10 s",
+        price: 580,
+    },
+    {
+        title: "Iphone 11",
+        image: i11,
+        description: "Awesome iphone 11",
+        price: 680,
+    },
+    {
+        title: "Iphone 11 pro",
+        image: i11pro,
+        description: "Awesome iphone 11 pro",
+        price: 800,
+    },
+    {
+        title: "Iphone 12",
+        image: i12,
+        description: "Awesome iphone 12",
+        price: 880,
+    },
+    {
+        title: "Iphone 12 pro",
+        image: i12pro,
+        description: "Awesome iphone 12 pro",
+        price: 950,
+    },
+    {
+        title: "Iphone 13",
+        image: i13,
+        description: "Awesome iphone 13",
+        price: 1050,
+    },
+    {
+        title: "Iphone 13 mini",
+        image: i13mini,
+        description: "Awesome iphone 13 mini",
+        price: 880,
+    },
+    {
+        title: "Iphone 13 pro",
+        image: i13pro,
+        description: "Awesome iphone 13 pro",
+        price: 1150,
+    },
+];
+
+export default products;
---
+++ src/data/productsImages.js
@@ -0,0 +1,37 @@
+import i6 from "../images/products/iphone-6.jpg";
+import i6s from "../images/products/iphone-6s.jpg";
+import i6splus from "../images/products/iphone-6s-plus.jpeg";
+import i7 from "../images/products/iphone-7.jpg";
+import i7plus from "../images/products/iphone-7-plus.jpg";
+import i8 from "../images/products/iphone-8.jpg";
+import i8plus from "../images/products/iphone-8-plus.avif";
+import i9 from "../images/products/iphone-9.jpg";
+import i10 from "../images/products/iphone-10.jpg";
+import i10s from "../images/products/iphone-10s.jpg";
+import i11 from "../images/products/iphone-11.jpg";
+import i11pro from "../images/products/iphone-11-pro.avif";
+import i12 from "../images/products/iphone-12.jpg";
+import i12pro from "../images/products/iphone-12-pro.jpg";
+import i13 from "../images/products/iphone-13.jpg";
+import i13mini from "../images/products/iphone-13-mini.jpg";
+import i13pro from "../images/products/iphone-13-pro.jpg";
+
+export {
+    i6,
+    i6s,
+    i6splus,
+    i7,
+    i7plus,
+    i8,
+    i8plus,
+    i9,
+    i10,
+    i10s,
+    i11,
+    i11pro,
+    i12,
+    i12pro,
+    i13,
+    i13mini,
+    i13pro,
+};
