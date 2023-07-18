8ab4a1eb6adf3b44e5fbafd27bbb33207bfbe340
janko12 <henrich.sury97@gmail.com>
2023-07-18 14:26:58 +0200

    Add products to product page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,3 +1,25 @@
+import styles from "./Shop.module.css";
+import products from "../data/products";
+
 export default function Shop() {
-    return <div>Shop</div>;
+    return (
+        <div className={styles.container}>
+            {products.map((product) => (
+                <Product key={product.description} {...product} />
+            ))}
+        </div>
+    );
+}
+
+function Product({ title, description, image, price }) {
+    return (
+        <div className={styles.product}>
+            <span className={styles["product-title"]}>{title}</span>
+            <img src={image} alt={title} />
+            <p className={styles["product-description"]}>
+                {description}
+            </p>
+            <span className={styles["product-price"]}>{price}</span>
+        </div>
+    );
 }
