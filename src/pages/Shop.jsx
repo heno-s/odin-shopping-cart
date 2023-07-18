9f39ff32a458f39148dcf48e24601376b38d0180
janko12 <henrich.sury97@gmail.com>
2023-07-18 20:53:25 +0200

    Add state for count in product

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,16 @@
 import styles from "./Shop.module.css";
 import products from "../data/products";
+import { useEffect, useState } from "react";
 
 export default function Shop() {
     return (
         <div className={styles.container}>
             <div className={styles.products}>
                 {products.map((product) => (
-                    <Product key={product.description} {...product} />
+                    <Product
+                        key={product.description + product.image}
+                        {...product}
+                    />
                 ))}
             </div>
         </div>
@@ -14,6 +18,26 @@ export default function Shop() {
 }
 
 function Product({ title, description, image, price }) {
+    const [count, setCount] = useState("1");
+
+    useEffect(() => {
+        if (+count < 1 && count !== "") {
+            setCount("1");
+        }
+    }, [count]);
+
+    function increment() {
+        setCount(+count + 1 + "");
+    }
+
+    function decrement() {
+        setCount(+count - 1 + "");
+    }
+
+    function handleCountChange(evt) {
+        setCount(evt.target.value);
+    }
+
     return (
         <div className={styles.product}>
             <span className={styles["product-title"]}>{title}</span>
@@ -24,11 +48,20 @@ function Product({ title, description, image, price }) {
             <span className={styles["product-price"]}>{price} €</span>
             <form>
                 <div className={styles["product-controls"]}>
-                    <button type="button">-</button>
+                    <button type="button" onClick={decrement}>
+                        -
+                    </button>
                     <div className={styles["product-count"]}>
-                        <input type="number" required />
+                        <input
+                            type="number"
+                            required
+                            value={count}
+                            onChange={handleCountChange}
+                        />
                     </div>
-                    <button type="button">+</button>
+                    <button type="button" onClick={increment}>
+                        +
+                    </button>
                 </div>
 
                 <button className={styles["submit-button"]}>
