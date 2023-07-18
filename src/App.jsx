d40205bad64ba861a79ef6ee4250a017390bedea
janko12 <henrich.sury97@gmail.com>
2023-07-18 13:19:50 +0200

    Create header navbar markup and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import "./globalStyles.css";
 import styles from "./App.module.css";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faCartShopping,
+    faHouse,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className={styles.app}>
+            <header className={styles.navbar}>
+                <NavLink to="/">
+                    <FontAwesomeIcon icon={faHouse} />
+                </NavLink>
+                <NavLink to="/shop">shop</NavLink>
+                <NavLink to="/cart">
+                    <FontAwesomeIcon icon={faCartShopping} />
+                </NavLink>
+            </header>
+        </div>
+    );
 }
 
 export default App;
