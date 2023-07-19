9b72849cb56c7a29fe3ac932db236cd51d564caf
janko12 <henrich.sury97@gmail.com>
2023-07-19 19:17:24 +0200

    Add total information, style cart page content

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,31 +1,52 @@
+import styles from "./Cart.module.css";
+
 export default function Cart({ cartItems }) {
     return (
-        <div>
-            {cartItems.map((cartItem) => {
-                const items = [];
+        <div className={styles.container}>
+            <div className={styles.items}>
+                {cartItems.map((cartItem) => {
+                    const items = [];
+                    for (let i = 0; i < cartItem.count; i++) {
+                        items.push(
+                            <CartItem
+                                key={cartItem.id + i}
+                                title={cartItem.title}
+                                image={cartItem.image}
+                                price={cartItem.price}
+                            />
+                        );
+                    }
+                    return items;
+                })}
+            </div>
+            <div className={styles.total}>
+                <span>
+                    Total:{" "}
+                    <strong>
+                        {cartItems.reduce(
+                            (total, cartItem) =>
+                                total +
+                                cartItem.price * cartItem.count,
+                            0
+                        )}{" "}
+                        €
+                    </strong>
+                </span>
 
-                for (let i = 0; i < cartItem.count; i++) {
-                    items.push(
-                        <CartItem
-                            key={cartItem.id + i}
-                            title={cartItem.title}
-                            image={cartItem.image}
-                            price={cartItem.price}
-                        />
-                    );
-                }
-                return items;
-            })}
+                <form>
+                    <button>Pay</button>
+                </form>
+            </div>
         </div>
     );
 }
 
 function CartItem({ title, image, price }) {
     return (
-        <div>
+        <div className={styles.item}>
             <span>{title}</span>
             <img src={image} alt={title} />
-            <span>{price}</span>
+            <strong>{price} €</strong>
         </div>
     );
 }
