473b026042c97613e5bf1cf3b2185d9bf1bca0f4
janko12 <henrich.sury97@gmail.com>
2023-07-20 16:28:15 +0200

    Fix bug - cart link changes color when active

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 export default function Home() {
     return (
         <div className={styles.container}>
             <p className={styles.slogan}>Explore new innovations!</p>
             <button className={styles["cta-button"]}>
-                <NavLink to="/shop">Shop</NavLink>{" "}
+                <Link to="/shop">Shop</Link>{" "}
             </button>
         </div>
     );
